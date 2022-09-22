17174b1aeea769349b4506aa447332f268ef5afd
BrunoCampos <brunogalvaocampos@gmail.com>
2022-09-22 10:04:48 -0300

    feature(components/dashboard/empty-dash.vue): add options for admins when project is empty

--- components/dashboard/empty-dash.vue
+++ components/dashboard/empty-dash.vue
@@ -1,25 +1,31 @@
 <template>
-  <div class="h-[500px] max-w-3xl border shadow-md p-14 mx-auto mt-20">
-    <div v-if="user.user_type != 1">
-      <p>Nenhum projeto encontrado</p>
+  <div class="max-w-xl border shadow-md p-14 mx-auto mt-20">
+    <div v-if="user.user_type != 1" class="flex justify-center items-center">
+      <p class="text-xl">Nenhum projeto encontrado!</p>
     </div>
     <div v-else>
-      <p class="text-xl mb-10">Nenhum projeto encontrado</p>
+      <p class="text-xl mb-10">Nenhum projeto encontrado!</p>
       <div
-        class="flex items-center gap-3 text-gray-600 ml-10 hover:cursor-pointer"
+        class="flex items-center gap-3 text-gray-600 ml-10 hover:cursor-pointer hover:text-green-600"
         @click="$router.push('/projects')"
       >
         <FolderPlus class="w-8 h-8" />
-        <p class="text-xl">Criar um Projeto</p>
+        <p class="text-xl">Crie um projeto</p>
+      </div>
+      <div
+        class="flex items-center gap-3 text-gray-600 ml-10 hover:cursor-pointer hover:text-green-600 mt-10"
+        @click="$router.push('/users')"
+      >
+        <UserEdit class="w-8 h-8" />
+        <p class="text-xl">Gerencie usuários</p>
       </div>
     </div>
   </div>
 </template>
 <script setup>
 import { useStorage } from "vue3-storage";
-import { FolderPlus } from "@vicons/fa";
+import { FolderPlus, UserEdit } from "@vicons/fa";
 
 const storage = useStorage();
 const user = storage.getStorageSync("user");
-console.log(user.user_type);
 </script>
