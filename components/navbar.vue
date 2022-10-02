4bc087fe7397079622752ab97dde335468f321d8
BrunoCampos <brunogalvaocampos@gmail.com>
2022-10-02 12:41:28 -0300

    fix(components/navbar.vue): fix navbar hight

--- components/navbar.vue
+++ components/navbar.vue
@@ -1,40 +1,40 @@
 <template>
   <div class="h-1/2">
     <div
-        class="flex justify-between p-2 border-b z-40 shadow-sm sticky top-0 bg-white"
+      class="flex justify-between p-2 border-b z-40 shadow-sm sticky top-0 bg-white"
     >
       <div class="flex items-center justify-center">Starti NPS</div>
       <div>
-        <LazyUserOptions/>
+        <LazyUserOptions />
       </div>
     </div>
     <div class="h-full">
       <NLayout has-sider>
         <NLayoutSider
-            :collapsed="collapsed"
-            :collapsed-width="64"
-            :on-update:collapsed="changeIconPosition"
-            :width="240"
-            bordered
-            collapse-mode="width"
-            content-style="height: 100%"
-            show-trigger
-            @collapse="collapsed = true"
-            @expand="collapsed = false"
+          :collapsed="collapsed"
+          :collapsed-width="64"
+          :on-update:collapsed="changeIconPosition"
+          :width="240"
+          bordered
+          collapse-mode="width"
+          content-style="height: 100%"
+          show-trigger
+          @collapse="collapsed = true"
+          @expand="collapsed = false"
         >
           <div>
             <NMenu
-                v-model:value="activeKey"
-                :collapsed="collapsed"
-                :collapsed-icon-size="22"
-                :collapsed-width="64"
-                :options="menuOptions"
-                style="color: red"
+              v-model:value="activeKey"
+              :collapsed="collapsed"
+              :collapsed-icon-size="22"
+              :collapsed-width="64"
+              :options="menuOptions"
+              style="color: red"
             />
           </div>
         </NLayoutSider>
         <NLayout>
-          <slot/>
+          <slot />
         </NLayout>
       </NLayout>
     </div>
@@ -42,13 +42,13 @@
 </template>
 
 <script setup>
-import {ChartPie, Cogs, Envelope, UserFriends} from "@vicons/fa";
+import { ChartPie, Cogs, Envelope, UserFriends } from "@vicons/fa";
 
-import {h} from "vue";
-import {NIcon, NLayout, NLayoutSider, NMenu} from "naive-ui";
-import {RouterLink} from "vue-router";
-import {useStorage} from "vue3-storage";
-import {values} from "lodash";
+import { h } from "vue";
+import { NIcon, NLayout, NLayoutSider, NMenu } from "naive-ui";
+import { RouterLink } from "vue-router";
+import { useStorage } from "vue3-storage";
+import { values } from "lodash";
 
 const route = useRoute();
 const storage = useStorage();
@@ -60,58 +60,58 @@ const user = ref(null);
 user.value = storage.getStorageSync("user");
 
 const renderIcon = (icon) => {
-  return () => h(NIcon, null, {default: () => h(icon)});
+  return () => h(NIcon, null, { default: () => h(icon) });
 };
 
 const menuOptions = ref([
   {
     label: () =>
-        h(
-            RouterLink,
-            {
-              to: "/",
-            },
-            {default: () => "Dashboard"}
-        ),
+      h(
+        RouterLink,
+        {
+          to: "/",
+        },
+        { default: () => "Dashboard" }
+      ),
     key: "dashboard",
     icon: renderIcon(ChartPie),
     permission: "*",
   },
   {
     label: () =>
-        h(
-            RouterLink,
-            {
-              to: "/projects",
-            },
-            {default: () => "Projetos"}
-        ),
+      h(
+        RouterLink,
+        {
+          to: "/projects",
+        },
+        { default: () => "Projetos" }
+      ),
     key: "projects",
     icon: renderIcon(Cogs),
     permission: "*",
   },
   {
     label: () =>
-        h(
-            RouterLink,
-            {
-              to: "/users",
-            },
-            {default: () => "Usuários"}
-        ),
+      h(
+        RouterLink,
+        {
+          to: "/users",
+        },
+        { default: () => "Usuários" }
+      ),
     key: "users",
     icon: renderIcon(UserFriends),
     permission: 1,
   },
   {
     label: () =>
-        h(
-            RouterLink,
-            {
-              to: "/config-email",
-            },
-            {default: () => "Configuração de e-mail"}
-        ),
+      h(
+        RouterLink,
+        {
+          to: "/config-email",
+        },
+        { default: () => "Configuração de e-mail" }
+      ),
     key: "config-email",
     icon: renderIcon(Envelope),
     permission: 1,
@@ -119,14 +119,13 @@ const menuOptions = ref([
 ]);
 
 menuOptions.value = menuOptions.value.filter(
-    (menu) => menu.permission === user.value.user_type || menu.permission === "*"
+  (menu) => menu.permission === user.value.user_type || menu.permission === "*"
 );
 
 const changeIconPosition = (isCollapsed) => {
   const icon = document.getElementsByClassName("n-layout-toggle-button");
   icon[0].style.left = isCollapsed ? "38px" : "215px";
   const colorBg = document.getElementsByClassName("n-menu--vertical");
-  console.log(colorBg[0]);
   if (colorBg[0]) {
     colorBg[0].style.width = isCollapsed ? "63px" : "215px";
   }
@@ -147,9 +146,12 @@ onMounted(() => {
 
 <style lang="scss">
 .n-layout-sider-scroll-container {
-  // height: 100vh !important;
+  height: 100vh !important;
   position: fixed;
   overflow: hidden;
+}
+.n-layout-sider {
+  height: 100vh !important;
 }
 
 .n-layout-toggle-button {
